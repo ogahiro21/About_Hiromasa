d9ec3051006e559cd6fe346f08158408e3b69a30
ogahiro21 <hiromasaogawaa@gmail.com>
2021-06-14 15:54:14 +0900

    flowを修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,17 @@ import { far } from '@fortawesome/pro-regular-svg-icons';
 import { fal } from '@fortawesome/pro-light-svg-icons';
 import { fad } from '@fortawesome/pro-duotone-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { WhoAmI } from './components/WhoAmI';
 
 // 全てのFontAwesomeアイコンをライブラリに追加
 library.add(fas, far, fal, fad, fab);
 const Home: React.VFC = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <WhoAmI />
+    </>
+  );
 };
 
 export default Home;
